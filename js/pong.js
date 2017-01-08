9be959dacc3d1f00d7f3216eeff825635dffe2e9
avinashjairam <avinash.jairam@gmail.com>
2017-01-07 22:20:47 -0500

    created css file, drew a blue circle on the js canvas

--- js/pong.js
+++ js/pong.js
@@ -1,9 +1,28 @@
-$(document).ready(function() {
-	alert("hi");
+// $(document).ready(function() {
+// 	alert("hi");
 
-});
+// });
 
-$("#click").click(function(){
-	alert("hi");
+// $("#click").click(function(){
+// 	alert("hi");
 
-});
+// });
+
+// function message(){
+
+// 	alert("hello")
+// }
+
+function draw(){
+	var c = document.getElementById("canvas");
+
+	var ctx = c.getContext("2d");
+
+	ctx.beginPath();
+	ctx.arc(100,75,50,0,2 * Math.PI);
+	ctx.fillStyle="blue";
+	ctx.fill();
+
+
+	ctx.stroke();
+}
