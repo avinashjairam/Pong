0d13fe54d543cff26225775d8fd4dade5bb8a149
avinashjairam <avinash.jairam@gmail.com>
2017-01-10 23:44:43 -0500

    trying to attach key listeners to paddle - unsuccessful so far

--- js/pong.js
+++ js/pong.js
@@ -145,52 +145,52 @@ function movingBall(){
 $(document).keypress(function(event){
 	if(event.keyCode==37){
 		if(x > 20){
-			ctx.clearRect(0,0,canvas.width,canvas.height);
+			//ctx.clearRect(0,0,canvas.width,canvas.height);
 			x-=5;
-			ctx.beginPath();
-			ctx.arc(x,y,20,0,2 * Math.PI);
-			ctx.fillStyle="blue";
-			ctx.fill();
-			ctx.closePath();
-			ctx.stroke();
+			// ctx.beginPath();
+			// ctx.arc(x,y,20,0,2 * Math.PI);
+			// ctx.fillStyle="blue";
+			// ctx.fill();
+			// ctx.closePath();
+			// ctx.stroke();
 		}
 	}
 	if(event.keyCode==38){
 		if(y > 20){
-			ctx.clearRect(0,0,canvas.width,canvas.height);
+			//ctx.clearRect(0,0,canvas.width,canvas.height);
 			y-=5;
-			ctx.beginPath();
-			ctx.arc(x,y,20,0,2 * Math.PI);
-			ctx.fillStyle="blue";
-			ctx.fill();
-			ctx.closePath();
-			ctx.stroke();
+			// ctx.beginPath();
+			// ctx.arc(x,y,20,0,2 * Math.PI);
+			// ctx.fillStyle="blue";
+			// ctx.fill();
+			// ctx.closePath();
+			// ctx.stroke();
 		}
 			
 	}
 
 	if(event.keyCode==39){
 		if( x < canvas.width -20){
-			ctx.clearRect(0,0,canvas.width,canvas.height);
+			//ctx.clearRect(0,0,canvas.width,canvas.height);
 			x+=5;
-			ctx.beginPath();
-			ctx.arc(x,y,20,0,2 * Math.PI);
-			ctx.fillStyle="blue";
-			ctx.fill();
-			ctx.closePath();
-			ctx.stroke();
+			// ctx.beginPath();
+			// ctx.arc(x,y,20,0,2 * Math.PI);
+			// ctx.fillStyle="blue";
+			// ctx.fill();
+			// ctx.closePath();
+			// ctx.stroke();
 		}
 	}
 	if(event.keyCode==40){
 		if ( y < canvas.height -20){
-			ctx.clearRect(0,0,canvas.width,canvas.height);
+			//ctx.clearRect(0,0,canvas.width,canvas.height);
 			y+=5;
-			ctx.beginPath();
-			ctx.arc(x,y,20,0,2 * Math.PI);
-			ctx.fillStyle="blue";
-			ctx.fill();
-			ctx.closePath();
-			ctx.stroke();
+			// ctx.beginPath();
+			// ctx.arc(x,y,20,0,2 * Math.PI);
+			// ctx.fillStyle="blue";
+			// ctx.fill();
+			// ctx.closePath();
+			// ctx.stroke();
 		}
 	}
 
