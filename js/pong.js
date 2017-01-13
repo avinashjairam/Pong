8e2e5efc5c809bb6737b3993b4601e0f451a1d12
avinashjairam <avinash.jairam@gmail.com>
2017-01-12 23:02:52 -0500

    ball and paddle are now moving. paddle is responding to key presses but dissappers are being pressed

--- js/pong.js
+++ js/pong.js
@@ -1,10 +1,12 @@
 
+
+
 var c = document.getElementById("canvas");
 
 var ctx = c.getContext("2d");
 
-var x = 40;
-var y = 40;
+var x = 10;
+var y = canvas.height/2;
 
 var count = 0; 
 
@@ -16,6 +18,13 @@ var xPos = canvas.width-10;
 var yPos = canvas.height/2; 
 var left = false;
 var right = true;
+
+
+
+
+
+
+
 
 function drawBall(){
 	ctx.beginPath();
@@ -30,17 +39,18 @@ function drawBall(){
 
 function drawPaddle(){
 	ctx.beginPath();
-	ctx.rect(10,canvas.height/2,10,40);
+	ctx.rect(x,y,10,40);
 	ctx.fillStyle="blue";
 	ctx.fill();
 	ctx.closePath();
+	//alert("drawPaddel callded");
 }
 
 
 
 
 function draw(){
-
+	//alert("hi");
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 
 	drawPaddle();
@@ -70,131 +80,27 @@ function draw(){
 		}
 	}
 
-	// ctx.beginPath();
 
-	// ctx.rect(0,0,canvas.width,canvas.height);
-	// ctx.fillStyle="green";
-	// ctx.fill();
-
-	// ctx.arc(x,y,20,0,2 * Math.PI);
-	// ctx.fillStyle="blue";
-	// ctx.fill();
-	// ctx.closePath();
-
-
-	// ctx.stroke();
-
-	// setInterval(movingBall,10);
-}
-
-// function moveBall(){
-// 	while(true){
-// 		ctx.clearArc(100,75,50,0,2 * Math.PI);
-// 		ctx.arc(100,75,50,0,2 * Math.PI);
-// 	ctx.fillStyle="blue";
-// 	ctx.fill();
-
-// 	}
-
-// }
-
-
-function movingBall(){
-
-	//alert("hi");
-
-
-	 //ctx2.clearRect(xPos-20,yPos-20,40,40);
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-
-
-	ctx.beginPath();
-
-	ctx.arc(xPos,yPos,10,0,2*Math.PI);
-	ctx.fillStyle="red";
-	ctx.fill();
-	ctx.closePath();
-
-	ctx.stroke();
-
-	if(count%2==0){
-		left =true;
-		right=false;
-	}
-	else{
-		left =false;
-		right=true;
-	}
-
-	if(xPos  >= 20 && left==true){
-		xPos--;
-
-		if(xPos==20){
-			count++;
-		}
-	}
-
-	if(xPos<=canvas.width-20 && right==true){
-		xPos++;
-		if(xPos == canvas.width-20){
-			count++;
-		}
-	}
-}
-
-$(document).keypress(function(event){
-	if(event.keyCode==37){
-		if(x > 20){
-			//ctx.clearRect(0,0,canvas.width,canvas.height);
+	$(document).keypress(function(event){
+		if(event.keyCode==37){
 			x-=5;
-			// ctx.beginPath();
-			// ctx.arc(x,y,20,0,2 * Math.PI);
-			// ctx.fillStyle="blue";
-			// ctx.fill();
-			// ctx.closePath();
-			// ctx.stroke();
 		}
-	}
-	if(event.keyCode==38){
-		if(y > 20){
-			//ctx.clearRect(0,0,canvas.width,canvas.height);
+		if(event.keyCode==38){
 			y-=5;
-			// ctx.beginPath();
-			// ctx.arc(x,y,20,0,2 * Math.PI);
-			// ctx.fillStyle="blue";
-			// ctx.fill();
-			// ctx.closePath();
-			// ctx.stroke();
 		}
-			
-	}
 
-	if(event.keyCode==39){
-		if( x < canvas.width -20){
-			//ctx.clearRect(0,0,canvas.width,canvas.height);
+		if(event.keyCode==39){
 			x+=5;
-			// ctx.beginPath();
-			// ctx.arc(x,y,20,0,2 * Math.PI);
-			// ctx.fillStyle="blue";
-			// ctx.fill();
-			// ctx.closePath();
-			// ctx.stroke();
 		}
-	}
-	if(event.keyCode==40){
-		if ( y < canvas.height -20){
-			//ctx.clearRect(0,0,canvas.width,canvas.height);
+		if(event.keyCode==40){
 			y+=5;
-			// ctx.beginPath();
-			// ctx.arc(x,y,20,0,2 * Math.PI);
-			// ctx.fillStyle="blue";
-			// ctx.fill();
-			// ctx.closePath();
-			// ctx.stroke();
 		}
-	}
+	});
 
-//  alert(String.fromCharCode(event.which) + " " + event.keyCode); 
-});
 
-setInterval(draw(),10);
+}
+
+
+
+
+setInterval(draw,10);
