a505a66fcc11a93d3ab3441705f8c80e07cf453c
avinashjairam <avinash.jairam@gmail.com>
2017-01-17 23:17:54 -0500

    Game over message now displaying when player loses

--- js/pong.js
+++ js/pong.js
@@ -28,6 +28,7 @@ var bounceFromTop = false;
 var bounceFromBottom = false;
 
 var start=true;
+var lose = false;
 
 $(document).keydown(function(event){
 	
@@ -60,7 +61,7 @@ $(document).keyup(function(event){
 function lost(){
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 	ctx.font = "30px Arial";
-    ctx.fillText("You Lost!",canvas.width/2,canvas.height/2);
+    ctx.fillText("Game Over!",150,canvas.height/2);
 }
 
 
@@ -92,111 +93,119 @@ function drawPaddle(){
 
 function draw(){
 	//alert("hi");
-	ctx.clearRect(0,0,canvas.width,canvas.height);
 
-	drawPaddle();
-	drawBall();
-	//alert(x);
+	if(lose==false){
 
-	if(start==true){
-		yPos++;
-	}
+		ctx.clearRect(0,0,canvas.width,canvas.height);
 
-	if(count%2==0){
-		left =true;
-		right=false;
-	}
-	else{
-		left =false;
-		right=true;
-	}
+		drawPaddle();
+		drawBall();
+		//alert(x);
 
-	//Moving the ball left 
-	if(xPos  >= 10 && left==true){
-		xPos--;
+		if(start==true){
+			yPos++;
+		}
+
+		if(count%2==0){
+			left =true;
+			right=false;
+		}
+		else{
+			left =false;
+			right=true;
+		}
+
+		//Moving the ball left 
+		if(xPos  >= 10 && left==true){
+			xPos--;
 
 
-		//When the ball reaches x Position 10, increment a counter.
-		// When the counter is odd the ball moves right.
+			//When the ball reaches x Position 10, increment a counter.
+			// When the counter is odd the ball moves right.
+
+			if(xPos==10){
+				count++;
+			}
+		}
+
+		//Moving the Ball Right 
+		if(xPos<=canvas.width-10 && right==true){
+			xPos++;
+			if(xPos == canvas.width-10){
+				count++;
+			}
+		}
+
+		if(upPressed==true && y >= 0){
+			y-=5;
+			//alert("Hi");
+		}
+
+		if(downPressed==true && y <= canvas.height- 40){
+			y+=5;
+		}
+
+		console.log("x = " + x + " xPos= " + xPos + " y =" +y + " yPos =" + yPos);
+
+		if(Math.abs(x -xPos) <= 11 && Math.abs(y-yPos) <=50){
+			
+			if(upPressed == true){
+				upCollide=true;
+				//yPos-=10;
+
+			}
+
+			if(downPressed == true){
+				downCollide=true;
+				//yPos+=10;
+			}
+			count++;
+		//	alert("hit");
+		}
 
 		if(xPos==10){
-			count++;
-		}
-	}
-
-	//Moving the Ball Right 
-	if(xPos<=canvas.width-10 && right==true){
-		xPos++;
-		if(xPos == canvas.width-10){
-			count++;
-		}
-	}
-
-	if(upPressed==true && y >= 0){
-		y-=5;
-		//alert("Hi");
-	}
-
-	if(downPressed==true && y <= canvas.height- 40){
-		y+=5;
-	}
-
-	console.log("x = " + x + " xPos= " + xPos + " y =" +y + " yPos =" + yPos);
-
-	if(Math.abs(x -xPos) <= 11 && Math.abs(y-yPos) <=50){
-		
-		if(upPressed == true){
-			upCollide=true;
-			//yPos-=10;
+			//alert("miss");
+			lose=true;
 
 		}
 
-		if(downPressed == true){
-			downCollide=true;
-			//yPos+=10;
-		}
-		count++;
-	//	alert("hit");
-	}
 
-	if(xPos==10){
-		//alert("miss");
+		if(upCollide==true){
+			yPos--;
+		}
+		if(downCollide==true){
+			yPos++;
+		}
+
+		if(yPos == 10){
+		//	alert("bounce top");
+			bounceFromTop=true;
+			bounceFromBottom=false;
+			
+			upCollide=false;
+		}
+
+		if(yPos == canvas.height-10){
+			//alert("bounce bottom");
+			bounceFromBottom=true;
+			bounceFromTop=false;
+
+			downCollide=false;
+		}
+		if(bounceFromTop ==true){
+			yPos++;
+		}
+
+		if(bounceFromBottom == true){
+			yPos--;
+			start=false;
+		}
+
+	}
+	else{
 		lost();
-
 	}
-
-
-	if(upCollide==true){
-		yPos--;
-	}
-	if(downCollide==true){
-		yPos++;
-	}
-
-	if(yPos == 10){
-	//	alert("bounce top");
-		bounceFromTop=true;
-		bounceFromBottom=false;
-		
-		upCollide=false;
-	}
-
-	if(yPos == canvas.height-10){
-		//alert("bounce bottom");
-		bounceFromBottom=true;
-		bounceFromTop=false;
-
-		downCollide=false;
-	}
-	if(bounceFromTop ==true){
-		yPos++;
-	}
-
-	if(bounceFromBottom == true){
-		yPos--;
-		start=false;
-	}
-
+	
 }
 
 
