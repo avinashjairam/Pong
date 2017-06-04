e5e072ff6abfa4d29a9fdcb2d85f47f2c64ddc6c
avinashjairam <avinash.jairam@gmail.com>
2017-06-04 15:22:47 -0400

    ball is now bouncing from top to bottom and vice versa

--- js/pong2.js
+++ js/pong2.js
@@ -11,12 +11,14 @@ var paddleY = 50;
 //It is incremented each time the ball collides with the paddle
 var count = 0; 
 
+//ballX and ballY is the initial starting position of the ball
+var ballX = canvas.width-10;
+var ballY = canvas.height/2; 
 
-
-// var xPos = canvas.width-10;
-// var yPos = canvas.height/2; 
-// var left = false;
-// var right = true;
+//these are the flags which determine what direction the ball is going in
+//By default, left is true
+var left = true;
+var right = false;
 
 var xClick=1;
 var yClick=1;
@@ -26,8 +28,8 @@ var downPressed = false;
 // var upCollide=false;
 // var downCollide=false;
 
-// var bounceFromTop = false;
-// var bounceFromBottom = false;
+var bounceFromTop = true;
+var bounceFromBottom = false;
 
 // var ballSpeed=2;
 
@@ -99,7 +101,7 @@ function lost(){
 function drawBall(){
 	ctx.beginPath();
 
-	ctx.arc(xPos,yPos,10,0,2*Math.PI);
+	ctx.arc(ballX,ballY,10,0,2*Math.PI);
 	ctx.fillStyle="red";
 	ctx.fill();
 	ctx.closePath();
@@ -113,45 +115,79 @@ function drawPaddle(){
 	ctx.fillStyle="blue";
 	ctx.fill();
 	ctx.closePath();
-	//ctx.stroke();
-	//alert("drawPaddel callded");
 }
 
-
+//Moves the paddle up or down depending on which key is pressed
+//Increases or decreases the Y coordinates if up or down is pressed
 function movePaddle(){
+
 	//Moving the paddle up
-
-
 	if(upPressed && paddleY >= 0){
-		paddleY-=5;
-		//alert("Hi");
+		paddleY -= 5;
 	}
-
 	//Moving the paddle down
 	if(downPressed && paddleY <= canvas.height- 40){
-		paddleY+=5;
+		paddleY += 5;
 	}
 
+}
+
+function bounce(){
+	if(ballY == canvas.height - 10){
+		ballY -= 6;
+	}
+
+	if(ballY == 10){
+		ballY -= 1;
+	}
+}
+
+function moveBall(){
+	if(start){
+		if(ballX > 10 && left){
+			ballX -= 1;
+			
+		}
+
+		if(ballY == canvas.height - 10){
+			bounceFromBottom = true;
+			bounceFromTop = false;
+		}
+
+		if(ballY == 10){
+			bounceFromTop = true;
+			bounceFromBottom = false;
+		}
+
+		if(bounceFromBottom){
+			ballY -= 1;
+		}
+
+		if(bounceFromTop){
+			ballY += 1;
+		}
+
+	}
 }
 
 function draw(){
 	
 	if(lose === false){
 
-	//	console.log("xpos " + xPos + "left " +left + "count " + count);
+	//	console.log("ballX " + ballX + "left " +left + "count " + count);
 
 		ctx.clearRect(0,0,canvas.width,canvas.height);
 
 		drawPaddle();
-	//	drawBall();
-		//alert(x);
+		drawBall();
 		movePaddle();
+		moveBall();
 
 		
 
 
 		// if(start==true){
-		// 	yPos++;
+		// 	ballY++;
 		// }
 
 		// if(count%2==0){
@@ -166,16 +202,16 @@ function draw(){
 	//	console.log(ballSpeed);
 
 		//Moving the ball left 
-		// if(xPos  >= 10 && left==true){
-		// 	xPos-=ballSpeed;
-		// 	//xPos-=3;
+		// if(ballX  >= 10 && left==true){
+		// 	ballX-=ballSpeed;
+		// 	//ballX-=3;
 
-		// 	console.log("xpos " + xPos + "left is " + left + " right is " + right + " count is " + count );
+		// 	console.log("ballX " + ballX + "left is " + left + " right is " + right + " count is " + count );
 
 		// 	//When the ball reaches x Position 10, increment a counter.
 		// 	// When the counter is odd the ball moves right.
 
-		// 	if(xPos==10){
+		// 	if(ballX==10){
 		// 		left = false;
 		// 		right = true;
 		// 		count++;
@@ -183,14 +219,14 @@ function draw(){
 		// }
 
 		//Moving the Ball Right 
-		// if(xPos<=canvas.width-10 && right==true){
-		// 	xPos+=ballSpeed;
-		// 	//xPos+=3;
+		// if(ballX<=canvas.width-10 && right==true){
+		// 	ballX+=ballSpeed;
+		// 	//ballX+=3;
 		// 	//alert("error");
 		// 	var z = canvas.width -10;
-		// 	console.log("xpos " + xPos + "left is " + left + " right is " + right + " count is " + count + "canvas width " + z);
+		// 	console.log("ballX " + ballX + "left is " + left + " right is " + right + " count is " + count + "canvas width " + z);
 
-		// 	if(xPos == canvas.width-50){
+		// 	if(ballX == canvas.width-50){
 		// 		console.log("right wall hit");
 		// 		count++;
 		// 		right = false;
@@ -201,15 +237,15 @@ function draw(){
 
 
 		
-		//console.log("x = " + x + " xPos= " + xPos + " y =" +y + " yPos =" + yPos);
+		//console.log("x = " + x + " ballX= " + ballX + " y =" +y + " ballY =" + ballY);
 
-		// if(Math.abs(paddleX -xPos) <= 11 && Math.abs(paddleY-yPos) <=50){
+		// if(Math.abs(paddleX -ballX) <= 11 && Math.abs(paddleY-ballY) <=50){
 			
 		// 	if(upPressed == true){
 		// 		upPressed = false;
 		// 		upCollide=true;
 		// 		alert("paddle hit");
-		// 		//yPos-=10;
+		// 		//ballY-=10;
 
 		// 	}
 
@@ -219,7 +255,7 @@ function draw(){
 		// 	if(downPressed == true){
 		// 		downPressed = false;
 		// 		downCollide=true;
-		// 		//yPos+=10;
+		// 		//ballY+=10;
 		// 	}
 		// 	count++;
 		// //	alert("hit");
@@ -227,15 +263,15 @@ function draw(){
 			
 		
 
-		// 	// if(yPos==27){
+		// 	// if(ballY==27){
 		// 	// 	console.log("upCollide " + upCollide + " downCollide " + downCollide + "count " +count);
 		// 	// }
 
-		// //console.log(yPos);
+		// //console.log(ballY);
 		// 	//console.log("upCollide " + upCollide + " downCollide " + downCollide + "count " +count);
 		// }
 
-		// if(xPos==10){
+		// if(ballX==10){
 		// 	//alert("miss");
 		// 	lose=true;
 
@@ -243,13 +279,13 @@ function draw(){
 
 
 		// if(upCollide==true){
-		// 	yPos--;
+		// 	ballY--;
 		// }
 		// if(downCollide==true){
-		// 	yPos++;
+		// 	ballY++;
 		// }
 
-		// if(yPos == 10){
+		// if(ballY == 10){
 		// //	alert("bounce top");
 		// 	bounceFromTop=true;
 		// 	bounceFromBottom=false;
@@ -257,7 +293,7 @@ function draw(){
 		// 	upCollide=false;
 		// }
 
-		// if(yPos == canvas.height-10){
+		// if(ballY == canvas.height-10){
 		// 	//alert("bounce bottom");
 		// 	bounceFromBottom=true;
 		// 	bounceFromTop=false;
@@ -265,18 +301,18 @@ function draw(){
 		// 	downCollide=false;
 		// }
 		// if(bounceFromTop ==true){
-		// 	yPos++;
+		// 	ballY++;
 
 		// }
 
 		// if(bounceFromBottom == true){
-		// 	yPos--;
+		// 	ballY--;
 		// 	start=false;
 		// }
 
 	}
 	else{
-	//	console.log(xPos);
+	//	console.log(ballX);
 		lost();
 		playAgain();
 
@@ -285,7 +321,7 @@ function draw(){
 
 	}
 
-	//console.log("xPos " + " " + xPos + " yPos " + yPos );
+	//console.log("ballX " + " " + ballX + " ballY " + ballY );
 
 
 	
